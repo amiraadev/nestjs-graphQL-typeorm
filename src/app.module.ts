a5bec28f3eab4a1ee2530d6c9538895033333319
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-06 23:59:57 +0100

    6

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot()],
   controllers: [],
   providers: [],
 })
