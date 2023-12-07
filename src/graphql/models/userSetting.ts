68a091fbedf43ee201936d7f8a6773c7f817646d
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-07 12:11:30 +0100

    UserSetting

---
+++ src/graphql/models/userSetting.ts
@@ -0,0 +1,13 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class UserSetting {
+  @Field((type) => Int)
+  userId: number;
+
+  @Field({ defaultValue: false })
+  receiveNotifications: boolean;
+
+  @Field({ defaultValue: false })
+  receiveEmails: boolean;
+}
