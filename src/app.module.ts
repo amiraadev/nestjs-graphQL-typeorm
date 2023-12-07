a7976b6b1627d5f6ff39b83ba283a3d049ddd260
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-07 10:46:03 +0100

    GraphQLModule.forRoot<ApolloDriverConfig>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [GraphQLModule.forRoot()],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
